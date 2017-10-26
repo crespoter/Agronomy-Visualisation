ceb44eb1f312407b7b55ac585408cf53f6696322
crespoter <davidchristiek@gmail.com>
2017-10-26 11:20:28 +0530

    working visualisation

---
+++ landcontroller.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class landcontroller : MonoBehaviour {
+    public GameObject camera;
+    public string requestURL;
+    public Text uiDisplay;
+    public Text ButtonDisplay;
+    private string area1;
+    private string area2;
+    private string depth;
+    private string cropname1;
+    private string cropname2;
+    public void Start()
+    {
+        requestURL = Application.absoluteURL;
+        area1 = requestURL.Split('?')[1].Split('&')[0].Split('=')[1];
+        cropname1 = requestURL.Split('?')[1].Split('&')[1].Split('=')[1];
+        area2 = requestURL.Split('?')[1].Split('&')[2].Split('=')[1];
+        cropname2 = requestURL.Split('?')[1].Split('&')[3].Split('=')[1];
+        uiDisplay.text = cropname1 + " \n\n Distribution:\n" + (int.Parse(area1)*100.0 / (int.Parse(area1) + int.Parse(area2))) + " %\n\n" +
+            "Area : " + area1 + " Acres";
+        ButtonDisplay.text = "Show " + cropname2;
+    }
+	public void goto2()
+    {
+        if (camera.GetComponent<Animator>().GetBool("goto2") == false)
+        {
+            camera.GetComponent<Animator>().SetBool("goto2", true);
+            uiDisplay.text = cropname2 + " \n\n Distribution:\n" + (int.Parse(area2) * 100.0 / (int.Parse(area1) + int.Parse(area2))) + " %\n\n" +
+                "Area : " + area2 + " Acres";
+            ButtonDisplay.text = "Show " + cropname1;
+        }
+        else
+        {
+            goto1();
+        }
+    }
+    public void goto1()
+    {
+        camera.GetComponent<Animator>().SetBool("goto2", false);
+        uiDisplay.text = cropname1 + " \n\n Distribution:\n" + (int.Parse(area1)*100 / (int.Parse(area1) + int.Parse(area2))) + " %\n\n" +
+            "Area : " + area1 + " Acres";
+        ButtonDisplay.text = "Show " + cropname2;
+    }
+}
---
+++ wellcontroller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wellcontroller : MonoBehaviour {
+    public GameObject camera;
+    public GameObject depthcanvas;
+    public GameObject well;
+    public GameObject wateryieldObject;
+    public string requestURL;
+    public Text wateryeildText;
+    public Text Depth;
+    private string wateryield;
+    private string depth;
+    private Vector3 origPosition;
+    private void Start()
+    {
+        requestURL = Application.absoluteURL;
+        wateryield = requestURL.Split('?')[1].Split('&')[0].Split('=')[1];
+        depth = requestURL.Split('?')[1].Split('&')[1].Split('=')[1];
+        wateryeildText.text = "Water Yield \n" + wateryield + " L" ;
+        origPosition = well.transform.position;
+    }
+    public void changeToVerticalView()
+    {
+        camera.GetComponent<Animator>().SetBool("changeToVertical", true);
+        depthcanvas.SetActive(true);
+        wateryieldObject.SetActive(true);
+        wateryieldObject.transform.localScale = new Vector3(wateryieldObject.transform.localScale.x, wateryieldObject.transform.localScale.y * int.Parse(wateryield) / 100.0f, well.transform.localScale.z);
+        wateryieldObject.transform.Translate(new Vector3(0, wateryieldObject.transform.localPosition.y * (well.transform.localScale.y - 1), 0));
+        Depth.text = "Depth : \n " + depth + " m";
+        well.transform.localScale = new Vector3(well.transform.localScale.x, well.transform.localScale.y * int.Parse(depth) / 100.0f, well.transform.localScale.z);
+        well.transform.Translate(new Vector3(0, well.transform.localPosition.y * (well.transform.localScale.y-1) , 0));
+        wateryieldObject.GetComponent<Renderer>().material.color = new Color(0.0f,0.0f,int.Parse(wateryield)/100.0f);
+    }
+    public void changeToHorizontalView()
+    {
+        wateryieldObject.SetActive(false);
+        camera.GetComponent<Animator>().SetBool("changeToVertical", false);
+        depthcanvas.SetActive(false);
+        well.transform.localScale = new Vector3(1, 1, 1);
+        well.transform.position = origPosition;
+
+    }
+}
